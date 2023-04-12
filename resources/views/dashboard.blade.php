f5885c670a2ed81b27b3ac725a01fa9231cbb424
Shofiatul Ayu Anikaningrum <shofiaa060@gmail.com>
2023-04-12 21:05:11 +0700

    Tampilan Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,13 +27,19 @@
       <!-- small box -->
       <div class="small-box bg-info">
         <div class="inner">
-          <h3>69</h3>
-          <p>Foods</p>
+          <h3>Ice Cream</h3>
+          <p>Ice cream adalah sebuah makanan beku yang terbuat dari campuran susu, gula, dan
+            bahan lain seperti buah-buahan, cokelat, kacang-kacangan, atau rempah-rempah. 
+            Campuran ini kemudian diproses dengan mesin khusus yang mengaduknya secara terus-menerus 
+            sehingga membentuk tekstur yang lembut dan krimi. Ice cream biasanya disajikan dalam wadah kertas atau plastik, 
+            dan bisa dimakan langsung atau sebagai tambahan dalam berbagai jenis makanan atau minuman.
+            Ice cream sangat populer di seluruh dunia, terutama pada musim panas atau dalam acara-acara tertentu.
+          </p>
         </div>
         <div class="icon">
-          <i class="fas fa-hamburger"></i>
+          <i class="fas fa-ice-cream"></i>
         </div>
-        <a href="{{ url('/food') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        <a href="{{ url('/ice_cream') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
@@ -41,13 +47,17 @@
       <!-- small box -->
       <div class="small-box bg-info">
         <div class="inner">
-          <h3>96</h3>
-          <p>Drinks</p>
+          <h3>Data Pegawai</h3>
+          <p>Pegawai ice cream adalah seseorang yang bekerja di sebuah toko atau gerai ice cream dan bertanggung jawab untuk 
+            mengolah, menjual, dan melayani produk ice cream kepada pelanggan. Tugas-tugas utama dari seorang pegawai ice cream meliputi 
+            menyajikan menu ice cream kepada pelanggan, mengoperasikan mesin pembuat ice cream, membersihkan peralatan dan area kerja, 
+            menjaga stok bahan baku, menghitung pendapatan, serta memberikan pelayanan yang baik dan ramah kepada pelanggan.
+            Seorang pegawai ice cream juga perlu memiliki pengetahuan tentang berbagai macam rasa ice cream, topping, dan sirup.</p>
         </div>
         <div class="icon">
-          <i class="fas fa-cocktail"></i>
+          <i class="fas fa-child"></i>
         </div>
-        <a href="{{ url('/drink') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        <a href="{{ url('/pegawai') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
       </div>
     </div>
     <!-- ./col -->
